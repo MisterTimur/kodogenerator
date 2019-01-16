37cb796db8bb42c2a2e67a5af654102decad35de
Тимур Абдулов <hostingurifa@gmail.com>
2019-01-17 04:34:45 +0500

    Кошмарики

--- ConsoleApplication23/generator.h
+++ ConsoleApplication23/generator.h
@@ -4,4 +4,5 @@ extern unsigned int  G_ADR;// Адрес где находимся в масиве G_MEM в данный момент
 extern unsigned char G_MEM[Max_Siz_Mem];// Готовый код программы
 
 void Loader();
+void Create_IMA(const char * iNam);
 bool Init();
--- ConsoleApplication23/main.cpp
+++ ConsoleApplication23/main.cpp
@@ -6,19 +6,9 @@ int main()
 {   
 	Init();
 	// Гененрируем код 
-	G_ADR=0;
-	Loader();
-	G_ADR=0;
-	Loader();
-	// Записываем на диск                           
-	// Записываем создайм файл образ диска 
-	FILE * Tr_File = fopen("TimOs.ima","wb"); 
-	int kol=0;
-	fseek(Tr_File, 0, SEEK_SET);// Перемещаемся в начало файла 
-	do{                                     
-	fwrite(&G_MEM[kol], 128, 1, Tr_File);// Загружаем файл
-	kol=kol+128;                         
-	} while (kol<Max_Siz_Mem);      
-	fclose(Tr_File);  
-	printf("Compile Ok Size of %d!\n",G_ADR);
+	G_ADR=0;Loader();// Первый проход 
+	G_ADR=0;Loader();// Второй проход     
+    printf("Compile Ok Size of %d!\n",G_ADR);
+    Create_IMA("TimOs.ima");
+
 }
